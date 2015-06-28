420da4f35a5ed1050fb6090e59fa8cc0d6dff317
Benjamin Herr <ben@0x539.de>
2015-06-28 21:30:06 +0200

    use rust-error crate and try!

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,7 @@
 extern crate tempdir;
-extern crate finally;
+
+#[macro_use]
+extern crate error_type;
 
 use std::path::{Path, PathBuf};
 use std::env::args_os;
@@ -10,19 +12,62 @@ use tempdir::TempDir;
 
 mod unpack_format;
 use unpack_format::UnpackFormat;
+mod config;
 
-fn unpack(formats: &[UnpackFormat], path: &Path) {
+#[derive(Debug, PartialEq, Eq, PartialOrd, Ord)]
+pub struct BadPath {
+    path: PathBuf,
+}
+
+impl BadPath {
+    fn new<P: Into<PathBuf>>(p: P) -> BadPath {
+        BadPath { path: p.into() }
+    }
+}
+
+#[derive(Debug, PartialEq, Eq, PartialOrd, Ord)]
+pub struct BadFormat {
+    path: PathBuf,
+}
+
+impl BadFormat {
+    fn new<P: Into<PathBuf>>(p: P) -> BadFormat {
+        BadFormat { path: p.into() }
+    }
+}
+
+error_type! {
+    #[derive(Debug)]
+    pub enum UnpackError {
+        Io(std::io::Error) {
+            cause;
+        },
+        Arg(BadPath) {
+            desc (_e) "bad path";
+            disp (e, fmt)
+                write!(fmt, "bad path: {}", e.path.to_string_lossy());
+        },
+        Format(BadFormat) {
+            desc (_e) "bad format";
+            disp (e, fmt)
+                write!(fmt, "bad format: {}", e.path.to_string_lossy());
+        },
+        Child(std::process::ExitStatus) {
+            desc (_e) "child exited unsuccessfully";
+        }
+    }
+}
+
+fn unpack(formats: &[UnpackFormat], path: &Path) -> Result<(), UnpackError> {
     let file_name = match path.file_name() {
         Some(file_name) => file_name,
-        None => panic!("no file component")
+        None => return Err(From::from(BadPath::new(path)))
     };
 
-    let format = UnpackFormat::find(formats, file_name).unwrap();
+    let format = try!(UnpackFormat::find(formats, file_name).ok_or_else(
+        || <UnpackError as From<_>>::from(BadFormat::new(path))));
 
-    let dir = match TempDir::new_in(".", ".unpack") {
-        Ok(dir) => dir,
-        Err(e) => panic!(e)
-    };
+    let dir = try!(TempDir::new_in(".", ".unpack"));
 
     let mut adjusted_path = PathBuf::new();
     adjusted_path.push("..");
@@ -32,32 +77,34 @@ fn unpack(formats: &[UnpackFormat], path: &Path) {
     cmd.arg(&adjusted_path);
     cmd.current_dir(dir.path());
     cmd.stdout(std::process::Stdio::null());
-    let status = cmd.spawn().unwrap().wait().unwrap();
+    let status = try!(try!(cmd.spawn()).wait());
     if !status.success() {
-        panic!("child exited with status {}", status);
+        return Err(From::from(status));
     }
     let mut single_entry = None;
-    let mut iter = read_dir(dir.path()).unwrap();
+    let mut iter = try!(read_dir(dir.path()));
 
     if let Some(entry) = iter.next() {
         if iter.next().is_none() {
-            single_entry = Some(entry.unwrap().path());
+            single_entry = Some(try!(entry).path());
         }
     }
 
     if let Some(entry) = single_entry {
         let new_name = entry.components().next_back().unwrap().as_os_str();
-        rename(&entry, new_name).unwrap();
-        remove_dir(dir.path()).unwrap();
+        try!(rename(&entry, new_name));
+        try!(remove_dir(dir.path()));
         println!("unpacked into \"{}\"", new_name.to_string_lossy());
     } else {
         let name_str = file_name.to_str().unwrap();
         let extension_offset = name_str.len() - format.extension.len();
         let dest = &name_str[ .. extension_offset];
-        rename(dir.path(), dest).unwrap();
+        try!(rename(dir.path(), dest));
         println!("unpacked into \"{}\"", dest);
     }
     dir.into_path();
+
+    Ok(())
 }
 
 #[cfg_attr(test, allow(dead_code))]
@@ -78,6 +125,6 @@ fn main() {
     ];
 
     for arg in args_os().skip(1) {
-        unpack(&formats, arg.as_ref());
+        unpack(&formats, arg.as_ref()).unwrap();
     }
 }
